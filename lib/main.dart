d6f4601bad10ea45a7f0adae978ebcf35099a443
mdminhaziftekhar <mdminhaziftekhar@gmail.com>
2023-03-24 12:36:24 +0600

    SliverList widget

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,7 @@ import 'package:flutter_learning/screens/floatingbutton.dart';
 import 'package:flutter_learning/screens/form.dart';
 import 'package:flutter_learning/screens/popupmenu.dart';
 import 'package:flutter_learning/screens/sliver_appbar.dart';
+import 'package:flutter_learning/screens/sliver_list.dart';
 import 'package:flutter_learning/screens/table.dart';
 import 'package:flutter_learning/screens/timepicker.dart';
 
@@ -46,6 +47,6 @@ class MyWidget extends StatefulWidget {
 class _MyWidgetState extends State<MyWidget> {
   @override
   Widget build(BuildContext context) {
-    return SliverAppbarScreen();
+    return SliverListScreen();
   }
 }
---
+++ lib/screens/sliver_list.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class SliverListScreen extends StatelessWidget {
+  const SliverListScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Sliver List Screen'),
+      ),
+      body: CustomScrollView(
+        slivers: [
+          SliverList(
+            
+            delegate: SliverChildBuilderDelegate((context, index) {
+              return ListTile(
+
+                title: Text('Item ${index + 1}'),
+                tileColor: Colors.orange[100 * (index % 9 + 1)],
+              );
+            }),
+          ),
+        ],
+      ),
+    );
+  }
+}
